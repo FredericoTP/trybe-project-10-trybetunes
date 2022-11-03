1468458439a7d4e576095a38924947d3388528f3
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-11-03 15:02:44 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import Search from './pages/Search';
+import Album from './pages/Album';
+import Profile from './pages/Profile';
+import Favorites from './pages/Favorites';
+import NotFound from './pages/NotFound';
+import ProfileEdit from './pages/ProfileEdit';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <Switch>
+        <Route exact path="/album/:id" component={ Album } />
+        <Route exact path="/profile/edit" component={ ProfileEdit } />
+        <Route exact path="/favorites" component={ Favorites } />
+        <Route exact path="/profile" component={ Profile } />
+        <Route exact path="/search" component={ Search } />
+        <Route exact path="/" component={ Home } />
+        <Route exact path="*" component={ NotFound } />
+      </Switch>
+    );
   }
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-login">
+        <p>TrybeTunes</p>
+        <p>Home</p>
+      </div>
+    );
+  }
+}
+
+export default Home;
